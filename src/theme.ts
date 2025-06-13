b08e7964f24ee09722875c53f51293f518e26fe3
Ahn Honghyun <anh2073@naver.com>
2025-06-13 12:00:41 +0900

    Update Theme

--- src/theme.ts
+++ src/theme.ts
@@ -1,8 +1,8 @@
 import { DefaultTheme } from "styled-components";
 
 export const darkTheme: DefaultTheme = {
-  bgColor: "#2f3640",
-  textColor: "white",
-  accentColor: "#9c88ff",
+  bgColor: "#7f8fa6",
+  textColor: "#1A1A3A",
+  accentColor: "#273c75",
   cardBgColor: "transparent",
 };
