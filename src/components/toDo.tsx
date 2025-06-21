e0fce5b071b67e340115e60bd7ba6b69fb9cedea
Ahn Honghyun <anh2073@naver.com>
2025-06-21 23:25:08 +0900

    Update toDo

--- src/components/toDo.tsx
+++ src/components/toDo.tsx
@@ -1,41 +1,69 @@
 import { useSetRecoilState } from "recoil";
 import { Categories, IToDo, toDoState } from "../atoms";
 
+import styled from "styled-components";
+
+const ListItem = styled.li``;
+
+const Text = styled.span``;
+
+const ButtonGroup = styled.div``;
+
+const Button = styled.button``;
+
 function ToDo({ text, category, id }: IToDo) {
   const setToDos = useSetRecoilState(toDoState);
+
   const onClick = (event: React.MouseEvent<HTMLButtonElement>) => {
     const {
       currentTarget: { name },
     } = event;
+
     setToDos((oldToDos) => {
-      const targetIndex = oldToDos.findIndex((toDo) => toDo.id === id);
-      const newToDo = { text, id, category: name as any };
+      if (name === "delete") {
+        return oldToDos.filter((toDo) => toDo.id !== id);
+      }
+
+      const targetIDX = oldToDos.findIndex((toDo) => toDo.id === id);
+      const oldToDo = oldToDos[targetIDX];
+      const newToDo = { ...oldToDo, category: name as Categories };
+
       return [
-        ...oldToDos.slice(0, targetIndex),
+        ...oldToDos.slice(0, targetIDX),
         newToDo,
-        ...oldToDos.slice(targetIndex + 1),
+        ...oldToDos.slice(targetIDX + 1),
       ];
     });
   };
+
   return (
-    <li>
-      <span>{text}</span>
-      {category !== Categories.DOING && (
-        <button name={Categories.DOING} onClick={onClick}>
-          Doing
-        </button>
-      )}
-      {category !== Categories.TO_DO && (
-        <button name={Categories.TO_DO} onClick={onClick}>
-          To Do
-        </button>
-      )}
-      {category !== Categories.DONE && (
-        <button name={Categories.DONE} onClick={onClick}>
-          Done
-        </button>
-      )}
-    </li>
+    <ListItem>
+      <Text>{text}</Text>
+
+      <ButtonGroup>
+        {category !== Categories.DOING && (
+          <Button name={Categories.DOING} onClick={onClick}>
+            Doing
+          </Button>
+        )}
+
+        {category !== Categories.TO_DO && (
+          <Button name={Categories.TO_DO} onClick={onClick}>
+            To Do
+          </Button>
+        )}
+
+        {category !== Categories.DONE && (
+          <Button name={Categories.DONE} onClick={onClick}>
+            Done
+          </Button>
+        )}
+
+        <Button name="delete" onClick={onClick}>
+          Delete
+        </Button>
+      </ButtonGroup>
+    </ListItem>
   );
 }
 
