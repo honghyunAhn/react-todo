be5dc1a808096e8d4935019ded1e21a408c3a541
Ahn Honghyun <anh2073@naver.com>
2025-06-22 21:20:14 +0900

    Style list, text

--- src/components/toDo.tsx
+++ src/components/toDo.tsx
@@ -3,9 +3,22 @@ import { Categories, IToDo, toDoState } from "../atoms";
 
 import styled from "styled-components";
 
-const ListItem = styled.li``;
+const ListItem = styled.li`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 10px 15px;
+  margin: 10px 0;
+  border-radius: 5px;
+  background-color: ${(props) => props.theme.cardBgColor};
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+`;
 
-const Text = styled.span``;
+const Text = styled.span`
+  flex: 1;
+  font-size: 16px;
+  color: ${(props) => props.theme.textColor};
+`;
 
 const ButtonGroup = styled.div``;
 
