1b873f8a4aa579f54c26d251d4c50c38997c7eed
Ahn Honghyun <anh2073@naver.com>
2025-07-01 15:38:58 +0900

    Update ToDoList

--- src/components/ToDoList.tsx
+++ src/components/ToDoList.tsx
@@ -1,29 +1,58 @@
-import React from "react";
 import { useRecoilState, useRecoilValue } from "recoil";
 import { Categories, categoryState, toDoSelector } from "../atoms";
+
 import CreateToDo from "./CreateToDo";
 import ToDo from "./toDo";
+
+import styled from "styled-components";
+
+const Container = styled.div``;
+
+const Title = styled.h1``;
+
+const ToDoCard = styled.div``;
+const TaskList = styled.ul``;
+
+const Select = styled.select``;
+
+const CategoryContainer = styled.div``;
+
+const EmptyMessage = styled.div``;
 
 function ToDoList() {
   const toDos = useRecoilValue(toDoSelector);
   const [category, setCategory] = useRecoilState(categoryState);
+
   const onInput = (event: React.FormEvent<HTMLSelectElement>) => {
-    setCategory(event.currentTarget.value as any);
+    setCategory(event.currentTarget.value as Categories);
   };
+
   return (
-    <div>
-      <h1>To Dos</h1>
-      <hr />
-      <select value={category} onInput={onInput}>
+    <Container>
+      <Title>TO DO LIST</Title>
+
+      <Select value={category} onInput={onInput}>
         <option value={Categories.TO_DO}>To Do</option>
         <option value={Categories.DOING}>Doing</option>
         <option value={Categories.DONE}>Done</option>
-      </select>
+      </Select>
+
       <CreateToDo />
-      {toDos?.map((toDo) => (
-        <ToDo key={toDo.id} {...toDo} />
-      ))}
-    </div>
+
+      <CategoryContainer>
+        <ToDoCard>
+          <Title>To Do</Title>
+        </ToDoCard>
+
+        <ToDoCard>
+          <Title>Doing</Title>
+        </ToDoCard>
+
+        <ToDoCard>
+          <Title>Done</Title>
+        </ToDoCard>
+      </CategoryContainer>
+    </Container>
   );
 }
 
